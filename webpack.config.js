7c39fbc72bb143597ef20dc8577fdbbd7d70b720
jankuchmacz <jan.kuchmacz.programista2@gmail.com>
2024-03-01 08:31:56 +0100

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,57 @@
 //console.log(__dirname);
 const path = require('path');
 //console.log(path.join(__dirname, 'public'))
-
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 //entry point - we have to provide it, where webpack should start
 //output file - where we put it
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module:{
-       rules: [{
-        loader:'babel-loader',
-        test: /\.js$/, //only files which end with .js
-        exclude: /node_modules/ //exclude some files
-       },{
-        test:/\.s?css$/, //target all files that end with .scss
-        use: [
-            'style-loader',
-            'css-loader',
-            'sass-loader'
-        ]//thanks to use we can specify array of loaders
-       }] 
-    },
-    //SourceMap - using source maps makes debugging faster
-    //we get information where error occurs
-    devtool: 'eval-cheap-module-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
+
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
+    return {
+            entry: './src/app.js',
+            output: {
+                path: path.join(__dirname, 'public'),
+                filename: 'bundle.js'
+            },
+        module:{
+        rules: [{
+            loader:'babel-loader',
+            test: /\.js$/, //only files which end with .js
+            exclude: /node_modules/ //exclude some files
+        },{
+            test:/\.s?css$/, //target all files that end with .scss
+            use: CSSExtract.extract({
+                use: [
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            })
+            //thanks to use we can specify array of loaders
+        }] 
+        },
+        plugins: [
+            CSSExtract
+        ],
+        //SourceMap - using source maps makes debugging faster
+        //we get information where error occurs
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
     }
-};
+}
 //loader - how a file is transformed when webpack uses it for example transform it with babel
 
 
